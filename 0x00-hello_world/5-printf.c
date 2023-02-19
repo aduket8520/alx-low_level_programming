9e6a69e1571d86a1d6b0892b60a5d070e6daef8e
titus <titusaduket@gmail.com>
2023-02-19 13:14:05 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - entry
- * Return: always 0
+ * main - the entry point into our program
+ * Description: main is the function where all our code will live
+ * Return: 0 if successful
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
