28fad9bac8d5dc9c01865bfba9fd009b4dcd1e0f
titus <titusaduket@gmail.com>
2023-02-19 13:26:47 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - entry
- * return: 1
+ * main - the entry point into our program
+ * Description: main is the function where all our code will live
+ * Return: 0 if successful
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fprintf(stderr,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
+
