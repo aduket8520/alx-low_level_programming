7669b4502bcdbb516ba61ef75a7b3745fadcbc87
titus <titusaduket@gmail.com>
2023-02-19 13:17:13 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - entry
- * return: always 0
+ * main - the entry point into our program
+ * Description: main is the function where all our code will live
+ * Return: 0 if successful
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle.");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
