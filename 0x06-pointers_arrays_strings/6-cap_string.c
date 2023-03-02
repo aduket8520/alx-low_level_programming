649b332abe9a3e3c272eb7a01823ada543c52e5f
titus <titusaduket@gmail.com>
2023-03-02 14:51:11 -0800

    function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,41 +1,47 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * *cap_string - capitalizes all words of a string.
+ * cap_string - a function that capitalizes
+ *              all words of a string
  *
- * @str: hold character.
- * Return: string.
- */
-char *cap_string(char *str)
-{
-	int i;
+ * @s: pointer to char input array
+ *
+ * Return: the capitalised string
+*/
 
-	for (i = 0; str[i] != '\0'; i++)
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	/*iterate through our array values*/
+	while (s[i] != '\0')
 	{
-		if (i == 0)
+		/*check for any lowercase letters*/
+		if (s[i] >= 97 && s[i] <= 122)
 		{
-			if ((str[i] >= 97 && str[i] <= 122))
+			/**
+			 * if we have a null character
+			 * change its value to capital
+			*/
+			if (i == 0)
 			{
-				str[i] = str[i] - 32;
-				continue;
+				s[i] -= 32;
+			}
+			/**
+			 * if we find any character matching the below before any small
+			 * letter we change that value to a capital letter.
+			*/
+			if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 ||
+				s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 ||
+				s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 ||
+				s[i - 1] == 40 || s[i - 1] == 41 || s[i - 1] == 123 ||
+				s[i - 1] == 124)
+			{
+				s[i] -= 32;
 			}
 		}
-		else if (str[i] == ' ')
-		{
-			++i;
-			if (str[i] >= 97 && str[i] <= 122)
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
-		}
-		else
-		{
-			if (str[i] >= 65 && str[i] <= 90)
-			{
-				str[i] = str[i] + 32;
-			}
-		}
+		i++;
 	}
-	return (str);
+	return (s);
 }
