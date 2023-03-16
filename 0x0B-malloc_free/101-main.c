d09eee51d158a913f2e0f8722e0c1b492bf295f0
titus <titusaduket@gmail.com>
2023-03-16 10:34:27 -0700

    all i have ever did

--- 0x0B-malloc_free/101-main.c
+++ 0x0B-malloc_free/101-main.c
@@ -27,7 +27,7 @@ int main(void)
 {
     char **tab;
 
-    tab = strtow("      ALX School         #cisfun      ");
+    tab = strtoq("      ALX School         #cisfun      ");
     if (tab == NULL)
     {
         printf("Failed\n");
