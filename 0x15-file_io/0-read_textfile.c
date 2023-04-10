20a64e0d246b17ad1d4f97f8ae12c35ecd4adc27
titus <titusaduket@gmail.com>
2023-04-10 12:52:38 +0300

    the first file and description

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+  ssize_t bytes_read = 0;
+  char *buffer = NULL;
+  FILE *file = NULL;
+  
+  if (filename == NULL)
+  {
+    return 0;
+  }
+
+  file = fopen(filename, "r");
+  if (file == NULL)
+  {
+    return 0;
+  }
+
+  buffer = (char *)malloc(letters + 1);
+  if (buffer == NULL)
+  {
+    fclose(file);
+    return 0;
+  }
+
+  bytes_read = fread(buffer, sizeof(char), letters, file);
+  if (bytes_read == -1)
+  {
+    free(buffer);
+    fclose(file);
+  }
+
+  buffer[bytes_read] = '\0';
+
+  if (!fwrite(buffer, sizeof(char), bytes_read, stdout))
+  {
+    free(buffer);
+    fclose(file);
+    return 0;
+  }
+
+  free(buffer);
+  fclose(file);
+
+  return bytes_read;
+}
